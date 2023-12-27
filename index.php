afa4c1f020e1b7d01edc4ff7d1bcbb5b204643c9
Belgacem Khadher <bkhader@axecibles.com>
2023-12-27 12:26:45 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<h1>Ce ci est un titre</h1>
+
+<!-- Utilise la camera frontale en mode photo -->
+<input type="file" id="selfie" accept="image/*" capture="user" />
+
+<!-- Utilise la camera extérieure en mode photo -->
+<input type="file" id="picture" accept="image/*" capture="environment" />
+
+<!-- Utilise la camera frontale en mode video -->
+<input type="file" id="selfievideo" accept="video/*" capture="user" />
+
+<!-- Utilise la camera extérieure en mode video -->
+<input type="file" id="video" accept="video/*" capture="environment" />
